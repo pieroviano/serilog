ab8aa9c4c7770fdd5635647741c8b820fdba3650
Piero Viano <piero.viano@gmail.com>
2023-04-11 06:29:08 +0300

    ''

---
+++ Build.bat
@@ -0,0 +1,10 @@
+del ..\WhenTheVersion\Packages\Net4X.Serilog.*
+rmdir /s /q %userprofile%\.nuget\Packages\Net4X.Serilog
+MSBuild.exe Serilog.sln -t:clean
+MSBuild.exe Serilog.sln -t:restore -p:RestorePackagesConfig=true
+msbuild Serilog.sln -t:restore -p:RestorePackagesConfig=true
+MSBuild.exe Serilog.sln -m /property:Configuration=%Configuration% 
+copy "src\Serilog\bin\%Configuration%\Net4X.Serilog.*" ..\WhenTheVersion\Packages\
+git add -A
+git commit -a --allow-empty-message -m ''
+git push
