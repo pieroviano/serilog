87fb7a65fe0b9c2ead2c071c52a5df51212d8d54
Piero Viano <piero.viano@gmail.com>
2023-04-11 06:48:51 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -5,7 +5,6 @@ MSBuild.exe Serilog.sln -t:clean
 MSBuild.exe Serilog.sln -t:restore -p:RestorePackagesConfig=true
 msbuild Serilog.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe Serilog.sln -m /property:Configuration=%Configuration% 
-copy "src\Serilog\bin\%Configuration%\Net4X.Serilog.*" ..\WhenTheVersion\Packages\
 git add -A
 git commit -a --allow-empty-message -m ''
 git push
